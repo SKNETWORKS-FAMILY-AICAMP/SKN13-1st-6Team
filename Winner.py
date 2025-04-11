8ad929b7a41177206d60105e824dc1ec30a05bf1
jiyun kang <jiyun_kang@ewha.ac.kr>
2025-04-11 10:48:03 +0900

    [지윤] 버튼 이미지 추가

---
+++ Winner.py
@@ -0,0 +1,11 @@
+class Winner:
+    def __init__(self, car_id:int = 0, model:str = "", img_url:str="", price: int =0, outfit: str="", win_log=0)
+        
+        self.car_id = car_id
+        self.model = model
+        self.img_url = img_url
+        self.price = price
+        self.outfit = outfit
+        self.win_log = win_log
+
+    
